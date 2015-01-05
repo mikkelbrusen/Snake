bd8d0feaaeacc417fc5bab5acad8da4a3ff1a2d2
BusterK <BusterK@10.16.175.245>
2015-01-05 12:14:54 +0100

    Changed Field.java to use movement procedure instead of setting a fixed position. Added Snake.java, with walk capabilities

--- Snake/src/model/Snake.java
+++ Snake/src/model/Snake.java
@@ -10,5 +10,36 @@ package model;
  * @author BusterK
  */
 public class Snake {
+    Field currentPosition;
     
+    public void move(char dir){
+        switch(dir){
+            case 'N':
+                walk(0, 1);
+                break;
+            case 'S':
+                walk(0, -1);
+                break;
+            case 'E':
+                walk(1, 0);
+                break;
+            case 'W':
+                walk(-1, 0);
+                break;
+        }        
+    }
+    private void walk(int x, int y){
+        if(isReverseDirection(x,y))
+            currentPosition.Move(0, 0);
+        else
+            currentPosition.Move(x, y);
+    }
+    
+    private boolean isReverseDirection(int x, int y){
+        if ( x == currentPosition.getX()-1 || 
+                y == currentPosition.getY()-1 )
+            return true;
+        else
+            return false;
+    }
 }
