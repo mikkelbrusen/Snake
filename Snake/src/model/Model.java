d86a6905e5b5e4ba7cb699f74f51c7c930d512d3
BusterK <BusterK@10.16.175.245>
2015-01-05 15:41:50 +0100

    Bla bla bla, et eller andet smart. JEG HAR LIGE FÅET 5.000 I FORSIKRING! -Glemte model.java

--- Snake/src/model/Model.java
+++ Snake/src/model/Model.java
@@ -7,9 +7,44 @@ package model;
 public class Model {
     private char[][] field;
     private Snake snake;
+    private Apple apple;
     
     public Model(int x, int y){
         this.field = new char[x][y];
-        this.snake = new Snake(x/2,y/2);
+        this.snake = new Snake(x/2,y/2,this);
+        this.apple = new Apple((int)Math.random()*x,(int)Math.random()*y);
+    }
+    
+    public int getAppleX(){
+        return apple.getX();
+    }
+    
+    public int getAppleY(){
+        return apple.getY();
+    }
+    
+    public void newApple(){
+        
+    }
+}
+class Dimension{
+    
+}
+
+class Apple{
+    private int x;
+    private int y;
+    
+    public Apple(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
     }
 }
