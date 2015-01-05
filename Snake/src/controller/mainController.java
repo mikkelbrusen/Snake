88c76264dd74ed9a2c1dad0e14709e0c2348ba31
GustavMadslund <GustavMadslund@10.16.164.62>
2015-01-05 14:47:20 +0100

    Add char to syso in controller

--- Snake/src/controller/mainController.java
+++ Snake/src/controller/mainController.java
@@ -3,7 +3,7 @@ package controller;
 public class mainController {
 	
 	public static void main(String[] args) {
-		System.out.println("Git Is Fun");
+		System.out.println("Git Is FunShit");
 	}
 
 }
