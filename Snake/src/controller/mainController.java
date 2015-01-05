a2416e0f0211ebaaebc98536b17483e94f6658ac
Mikkel Møller Brusen <mikkel_mb@hotmail.com>
2015-01-05 14:51:18 +0100

    Mikkel vil også ændre i syso

--- Snake/src/controller/mainController.java
+++ Snake/src/controller/mainController.java
@@ -3,7 +3,7 @@ package controller;
 public class mainController {
 	
 	public static void main(String[] args) {
-		System.out.println("Git Is FunShit");
+		System.out.println("Git Is FunMIKKEL");
 	}
 
 }
