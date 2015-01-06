12127f0050ef4d36b02ebf9714f2550738c51bc9
BusterK <BusterK@10.16.175.252>
2015-01-06 13:19:51 +0100

    removed comments

--- Snake/src/view/View.java
+++ Snake/src/view/View.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 import java.awt.BorderLayout;
@@ -10,10 +5,6 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-/**
- *
- * @author BusterK
- */
 public class View extends JFrame {
 	
 	private JPanel snakePanel;
