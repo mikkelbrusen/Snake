852c2eac2426bc105693ab4d48e7d9caabaa1c9b
Mikkel Møller Brusen <mikkel_mb@hotmail.com>
2015-01-08 16:43:52 +0100

    Move immages

--- Snake/src/controller/Controller.java
+++ Snake/src/controller/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
         private final static int INTERVAL = 50;
 	private Model model;
 	private View view;
-        private Dimension dimension;
+    private Dimension dimension;
 	
 	public Controller(Dimension dimension,File track) {
             this.dimension = dimension;
